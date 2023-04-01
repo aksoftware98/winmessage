5688554a786dadf2c333f47dc7882c823367b341
Ahmad Mozaffar <aksoftware98@hotmail.com>
2023-04-01 10:26:50 +0400

    Design a sample chat page

--- src/WinMessage.Windows/WinMessage.Windows/Pages/ChatPage.xaml.cs
+++ src/WinMessage.Windows/WinMessage.Windows/Pages/ChatPage.xaml.cs
@@ -29,6 +29,8 @@ namespace WinMessage.Windows.Pages
 		{
 			this.InitializeComponent();
 			InitializeConversations();
+			InitializeMessages();
+			ChatShadow.Receivers.Add(ProfileDetailsGrid);
 		}
 
 
@@ -54,7 +56,7 @@ namespace WinMessage.Windows.Pages
 			});
 			Conversations.Add(new Conversation
 			{
-				DisplayName = "John Smith",
+				DisplayName = "Adrien Smith",
 				Id = Guid.NewGuid().ToString(),
 				LastMessageTime = "Yesterday",
 				LatestMessage = "Bye bye!",
@@ -68,6 +70,162 @@ namespace WinMessage.Windows.Pages
 				LatestMessage = "🚀🚀🚀🚀",
 				ProfilePictureUrl = @"ms-appx:///Assets/Images/person-4.png"
 			});
+		}
+
+		public List<Message> Messages { get; set; } = new();
+
+		private void InitializeMessages()
+		{
+			Messages.Add(new Message
+			{
+				IsSent = false,
+				Id = Guid.NewGuid().ToString(),
+				ProfilePictureUrl = null,
+				Content = "Hi, how are you?",
+				IsImage = false,
+				Time = "11:40 PM"
+			});
+			Messages.Add(new Message
+			{
+				IsSent = false,
+				Id = Guid.NewGuid().ToString(),
+				ProfilePictureUrl = null,
+				Content = "I'm soo curios to know how the work is going!!!!!",
+				IsImage = false,
+				Time = "11:40 PM"
+			});
+			
+			Messages.Add(new Message
+			{
+				IsSent = true,
+				Id = Guid.NewGuid().ToString(),
+				ProfilePictureUrl = null,
+				Content = "Heey Sarah!!",
+				IsImage = false,
+				Time = "11:40 PM"
+			});
+			Messages.Add(new Message
+			{
+				IsSent = false,
+				Id = Guid.NewGuid().ToString(),
+				ProfilePictureUrl = null,
+				Content = "How is the design?",
+				IsImage = false,
+				Time = "11:40 PM"
+			});
+			Messages.Add(new Message
+			{
+				IsSent = true,
+				Id = Guid.NewGuid().ToString(),
+				ProfilePictureUrl = null,
+				Content = "It's going crazy 😍🤣😍🤣",
+				IsImage = false,
+				Time = "11:40 PM"
+			});
+			Messages.Add(new Message
+			{
+				IsSent = true,
+				Id = Guid.NewGuid().ToString(),
+				ProfilePictureUrl = null,
+				Content = "Designing Windows 11 stuff is amaaazing!!!!",
+				IsImage = false,
+				Time = "11:40 PM"
+			});
+			Messages.Add(new Message
+			{
+				IsSent = false,
+				Id = Guid.NewGuid().ToString(),
+				ProfilePictureUrl = null,
+				Content = "Come on! send me some live screenshots!!",
+				IsImage = false,
+				Time = "11:41 PM"
+			});
+			Messages.Add(new Message
+			{
+				IsSent = true,
+				Id = Guid.NewGuid().ToString(),
+				ProfilePictureUrl = null,
+				Content = "Sure thing!",
+				IsImage = false,
+				Time = "11:41 PM"
+			});
+			Messages.Add(new Message
+			{
+				IsSent = true,
+				Id = Guid.NewGuid().ToString(),
+				ProfilePictureUrl = @"ms-appx:///Assets/Images/ChatImage.png",
+				Content = "Currently working on the new video feature",
+				IsImage = true,
+				Time = "11:42 PM"
+			});
+			Messages.Add(new Message
+			{
+				IsSent = false,
+				Id = Guid.NewGuid().ToString(),
+				ProfilePictureUrl = null,
+				Content = "Lovely! 😍 😍 Planning to support audio and video call??",
+				IsImage = false,
+				Time = "11:40 PM"
+			});
+			Messages.Add(new Message
+			{
+				IsSent = true,
+				Id = Guid.NewGuid().ToString(),
+				ProfilePictureUrl = null,
+				Content = "Currently working on the new video feature",
+				IsImage = false,
+				Time = "11:43 PM"
+			});
+
+		}
+	}
+
+	public class Message
+	{
+		public string Id { get; set; }
+
+		public bool IsSent { get; set; }
+
+		public bool IsImage { get; set; }
+
+		public string Content { get; set; }
+
+		public string Time { get; set; }
+
+		public string ProfilePictureUrl { get; set; }
+		
+	}
+
+	public class MessageTemplateSelector : DataTemplateSelector
+	{
+
+		public DataTemplate SentMessageDataTemplate { get; set; }
+
+		public DataTemplate ReceivedMessageDataTemplate { get; set; }
+
+		public DataTemplate SentImagesMessageDataTemplate { get; set; }
+
+		public DataTemplate ReceivedImagesMessageDataTemplate { get; set; }
+
+		protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+		{
+			var message = item as Message;
+			if (message.IsSent && !message.IsImage)
+			{
+				return SentMessageDataTemplate;
+			}
+			else if (message.IsSent && message.IsImage)
+			{
+				return SentImagesMessageDataTemplate;
+			}
+			else if (!message.IsSent && !message.IsImage)
+			{
+				return ReceivedMessageDataTemplate;
+			}
+			else
+			{
+				return ReceivedImagesMessageDataTemplate;
+			}
 		}
 	}
 }
